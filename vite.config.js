7026946e46fef08e4ca4fac2b49096e85b961c31
Julio Varas Contreras <jvarascontreras@gmail.com>
2026-05-22 20:02:06 -0400

    cambios para usar GH Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/' // Configuración para Netlify (raíz del dominio)
+  base: '/panini-tracker/' // ¡IMPORTANTE! Reemplaza 'panini-tracker' con el nombre exacto de tu repositorio en GitHub.
 })
